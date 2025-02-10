d587d87c945bee8c60676d8e57846b7031094d9d
Jarrett Keifer <jkeifer0@gmail.com>
2025-02-10 10:28:42 -0800

    implement source reading

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
-use bagelwithlox::reader;
+use std::io;
+use std::io::Error;
+use std::io::IsTerminal;
+use bagelwithlox::reader::Source;
 use bagelwithlox::interpreter::Interpreter;
 
 #[derive(Parser)]
@@ -7,12 +10,58 @@ use bagelwithlox::interpreter::Interpreter;
     version,
     about,
 )]
-struct Cli {}
+struct Cli {
+    #[arg(short)]
+    cmd: Option<String>,
+    file: Option<String>,
+}
+
+impl Cli {
+    fn get_source(&self) -> Option<Result<Source, Error>> {
+
+        if let Some(cmd) = &self.cmd {
+            eprintln!("Reading from command option");
+            return Some(Ok(Source::from_string(&cmd)));
+        }
+
+        if let Some(path) = &self.file {
+            eprintln!("Reading from file arg '{}'", &path);
+            return Some(Source::from_file(&path));
+        }
+
+        if io::stdin().is_terminal() {
+            return None;
+        }
+
+        eprintln!("Reading from stdin");
+        let stdin = match io::read_to_string(io::stdin()) {
+            Ok(string) => string,
+            Err(e) => return Some(Err(e)),
+        };
+
+        return Some(Ok(Source::from_string(&stdin)));
+    }
+}
+
+
+fn repl() {
+    eprintln!("Running the repl!");
+}
 
 
 fn main() {
-    let _cli = Cli::parse();
+    let cli = Cli::parse();
+    let interpreter =  Interpreter::new();
 
-    let src = reader::read_source("file.bwl");
-    Interpreter::new().interpret(src);
+    if let Some(src) = cli.get_source() {
+        match src {
+            Ok(src) => {
+                eprintln!("Got the following source content:\n'''\n{}\n'''", &src.get_content());
+                interpreter.interpret(src)
+            },
+            Err(e) => eprintln!("Encountered an error: {}", e),
+        }
+    } else {
+        repl()
+    }
 }
--- src/reader.rs
+++ src/reader.rs
@@ -1,7 +1,36 @@
-pub type Source = ();
+use std::io::Error;
+use std::fs;
 
-pub fn read_source(filename : &str) -> Source {
-    println!("Reading source '{}'...", &filename);
+pub struct Source {
+    filename: Option<String>,
+    content: String,
+}
+
+impl Source {
+    pub fn from_string(content: &str) -> Source {
+        Source{
+            filename: None,
+            content: content.to_string(),
+        }
+    }
+
+    pub fn from_file(path: &str) -> Result<Source, Error> {
+        Ok(Source{
+            filename: Some(path.to_string()),
+            content: fs::read_to_string(path)?,
+        })
+    }
+
+    pub fn get_filename(&self) -> Option<&str> {
+        match &self.filename {
+            Some(string) => Some(&string),
+            None => None,
+        }
+    }
+
+    pub fn get_content(&self) -> &str {
+        &self.content
+    }
 }
 
 #[cfg(test)]
@@ -10,6 +39,6 @@ mod tests {
 
     #[test]
     fn test_read_source() {
-        read_source("file.bwl");
+        Source::from_string("content");
     }
 }
